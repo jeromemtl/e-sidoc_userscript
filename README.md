83ef132dabde80bbdbbe3b09c5f098b1be83a9a2
jeromemtl <61432191+jeromemtl@users.noreply.github.com>
2021-08-29 22:10:18 +0400

    Ajout script exemplaire

--- README.md
+++ README.md
@@ -1,13 +1,30 @@
 # Userscript e-sidoc
 
-![Administrer le portail](https://jeromemtl.github.io/e-sidoc_userscript/image/exemple.png)
+## Bouton administrer le portail
 
-## A quoi sert ce script ?
+![Administrer le portail](https://jeromemtl.github.io/e-sidoc_userscript/image/administrer.png)
+
+### A quoi sert ce script ?
 
 Ajoute un élément "Administrer le portail" dans le menu "Mon compte".  
 Plus besoin de taper d'URL, vous pourrez accéder à l'administration de e-sidoc en 2 clics.
 
-## Comment l'utiliser ?
+### Comment l'utiliser ?
 
 * Installez l'extension [Tampermonkey](https://www.tampermonkey.net/) selon votre navigateur.
 * Cliquez sur le lien suivant pour installer le script : [Administrer le portail](https://jeromemtl.github.io/e-sidoc_userscript/e-sidoc_admin_btn.user.js).
+
+
+
+## Emplacements des exemplaires
+
+![Emplacements des exemplaires](https://jeromemtl.github.io/e-sidoc_userscript/image/exemplaire.png)
+
+### A quoi sert ce script ?
+
+Affiche l'emplacement, la cote et la disponnibilité des exemplaires dans le premier niveau de résultat de recherche.
+
+### Comment l'utiliser ?
+
+* Installez l'extension [Tampermonkey](https://www.tampermonkey.net/) selon votre navigateur.
+* Cliquez sur le lien suivant pour installer le script : [Emplacements des exemplairesl](https://jeromemtl.github.io/e-sidoc_userscript/e-sidoc_emplacement_cote_et_dispo.user.js).
---
+++ e-sidoc_emplacement_cote_et_dispo.user.js
@@ -0,0 +1,94 @@
+// ==UserScript==
+// @name         e-sidoc - emplacement, cote et disponnibilité
+// @namespace    http://www.twitter.com/jeromemtl
+// @version      0.1
+// @description  Affiche l'emplacement, la cote et la disponnibilité des exemplaires dans le premier niveau de résultat de recherche
+// @author       JeromeMTL
+// @match        https://*.esidoc.fr/
+// @match        https://*.esidoc.fr/recherche/*
+// @match        https://*.esidoc.fr/decouvrir/*
+// @match        https://*.esidoc.fr/voirplus/*
+// @exclude      https://administration.esidoc.fr/*
+// @exclude      https://authentification.esidoc.fr/*
+// @grant    GM_addStyle
+// @run-at       document-idle
+// @icon         https://frontend.esidoc.fr/img/favicon-32x32.png
+// @require      http://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js
+// @require      https://gist.github.com/raw/2625891/waitForKeyElements.js
+// ==/UserScript==
+
+GM_addStyle ( `
+/* Localiser le document, bouton orange */
+.search-results .content-buttons .disponibilite .btn-warning.document-nodispo div, #pop-content article.notice .content-buttons .disponibilite .btn-warning.document-nodispo div {
+    position: relative;
+    width: calc(100% + 20px);
+    text-align: center;
+    background-color: #fff;
+    color: #4cae4c;
+    left: -10px;
+    border-radius: 0 0 2px 2px;
+    max-height: 20px;
+    font-size: .9em;
+    margin: 0 !important;
+    bottom: -5px;
+}
+/* Alignement btn Réserver le document */
+article.notice .disponibilite .widget-reservations-search {
+    margin: 0 10px;
+}
+article.notice .disponibilite {
+    align-items: normal;
+}
+` );
+
+(function() {
+    'use strict';
+ 
+    waitForKeyElements(".table-guide", addEmplacement);
+
+    function addEmplacement(){
+
+        var nbResultatShow=document.getElementById('bloc-style').getElementsByClassName('list')[0].getElementsByClassName('search-result').length-1; //on évite le dernier élément
+
+        //pr chq résultat
+        for(var i=0; i <= nbResultatShow; i++){
+            //on esquive les ressources sans localisation/sans exemplaire
+            if(document.getElementsByClassName('search-result')[i].getElementsByTagName('td')[1]!==undefined){
+
+                //Si il y a qqch ds le btn "localiser le document", on le suppr
+                if(document.getElementsByClassName('disponibilite')[i].getElementsByClassName('btn')[0].getElementsByTagName('div')[0]!==undefined){
+                    while(document.getElementsByClassName('disponibilite')[i].getElementsByClassName('btn')[0].getElementsByTagName('div').length>0){
+                        document.getElementsByClassName('disponibilite')[i].getElementsByClassName('btn')[0].getElementsByTagName('div')[0].remove();
+                    }
+
+                }
+
+                var nbExemplaire=document.getElementsByClassName('search-result')[i].getElementsByTagName('tr').length-1;
+
+                //pr chq exemplaire (ls infos sont ttes les 3 celulles)
+                for(var j=0; j<(nbExemplaire*3); j+=3){
+                    //on recup ls infos des exemplaires
+                    var cote=document.getElementsByClassName('search-result')[i].getElementsByTagName('td')[j].innerText;
+                    var emplacement=document.getElementsByClassName('search-result')[i].getElementsByTagName('td')[j+1].innerText;
+                    var statut=document.getElementsByClassName('search-result')[i].getElementsByTagName('td')[j+2].innerText;
+
+                    //Change la couleur du texte selon le statut
+                    switch (statut) {
+                        case 'Disponible':
+                            var color=''; //on change rien (vert ou blanc)
+                            break;
+                        case 'Indisponible':
+                            var color='#f00'; //rouge
+                            break;
+                        default: //Document prêté ou Document réservé
+                            var color='#d97f00'; //orange
+                    }
+
+                    //et on remplace le contenu
+                    document.getElementsByClassName('disponibilite')[i].getElementsByClassName('btn')[0].innerHTML+='<div style="color:'+color+';"><strong>'+emplacement+'</strong> - '+cote+'</div>';
+
+                }
+            } 
+        }
+    }
+})();
